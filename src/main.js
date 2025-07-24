24692c280c94e8e94a841d10235c8a3207b6693e
saw <ehkalanephaung96@gmail.com>
2025-07-24 13:29:19 +0700

    add both box gemetry and plane gemetry

--- src/main.js
+++ src/main.js
@@ -1,4 +1,35 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-console.log('Scene created:', scene);
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
+const randerer = new THREE.WebGLRenderer()
+randerer.setSize(innerWidth, innerHeight)
+randerer.setPixelRatio(devicePixelRatio)
+randerer.setClearColor(0x000000, 1) // Set background color
+document.body.appendChild(randerer.domElement)
+
+// create geometry
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00FF00 })
+const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial)
+scene.add(boxMesh)
+randerer.render(scene, camera)
+camera.position.z = 5
+
+// palne geometry
+const planeGeometry = new THREE.PlaneGeometry(3,3,10,10)
+const planeMaterial = new THREE.MeshBasicMaterial({ color: 0x0000FF, side: THREE.DoubleSide })
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
+scene.add(planeMesh)
+
+
+function animate() {
+  requestAnimationFrame(animate)
+  randerer.render(scene, camera)
+  boxMesh.rotation.x += 0.01
+  boxMesh.rotation.y += 0.01
+    planeMesh.rotation.x += 0.01
+    planeMesh.rotation.y += 0.01
+}
+animate()
+
